6fffd9df2a271e546571becae4a42bfcfe9bd45e
joshua <youzeiteikoku@gmail.com>
2020-07-26 21:26:21 +0800

    moved files to respective folder app and main

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -3,7 +3,7 @@ module.exports = {
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: './src/main.js',
+  entry: './src/Main',
   // Put your normal webpack config below here
   module: {
     // eslint-disable-next-line global-require
